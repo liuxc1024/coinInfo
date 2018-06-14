fc168fe6e66b81601104e02abb3bd7c9cc6ccd53
liuxc1024 <xiangcenliu1992@163.com>
2018-06-14 16:52:58 +0800

    binance get list ok

--- DataMinner/binance/binance.py
+++ DataMinner/binance/binance.py
@@ -12,25 +12,32 @@ def get_trade_url_list(html_content):
         for cur_find in find:
             btc_trade_list_url.append(config.SITE_MAIN_ADDR["binance"] + cur_find)
         return btc_trade_list_url
-    except:
+    except :
         log.error("get btc trade list from binance error")
         return btc_trade_list_url
 
 
-def get_btc_trade_url_list():
+def get_data():
     # get btc trade list
     connPool = urllib3.PoolManager()
     http = connPool.request(method='get', url=config.SITE_MAIN_ADDR["binance"] + "/cn")
-    temp = etree.HTML(http.data)
-    btc_trade_list = get_trade_url_list(temp)
+    btc_trade_list = get_trade_url_list(http.data)
 
     # get bnb trade list
-    file = open("d:/coin/coinInfo/DataMinner/binanceBnb.txt", encoding="utf-8")
+    file = open("d:/coin/coinInfo/DataMinner/binance/binanceBnb.txt", encoding="utf-8")
     content = file.read()
     bnb_trade_list = get_trade_url_list(content)
 
-    # get
+    # get eth trade list
+    file = open("d:/coin/coinInfo/DataMinner/binance/binanceEth.txt", encoding="utf-8")
+    content = file.read()
+    eth_trade_list = get_trade_url_list(content)
+
+    # get usdt trade list
+    file = open("d:/coin/coinInfo/DataMinner/binance/binanceUsdt.txt", encoding="utf-8")
+    content = file.read()
+    usdt_trade_list = get_trade_url_list(content)
 
 
 if __name__ == "__main__":
-    None
+    get_data()
