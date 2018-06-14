014a42c46077810a26b1d537ad3a44ce2ea41316
liuxc2014 <xiangcenliu1992@163.com>
2018-06-15 03:43:04 +0800

    binance of

--- DataMinner/binance/binance.py
+++ DataMinner/binance/binance.py
@@ -1,7 +1,8 @@
 from lxml import etree
 import DataMinner.config as config
 import urllib3
-import logging as log
+import logging
+log = logging.getLogger("bianceDataGet")
 import DataMinner.common as common
 
 
@@ -11,20 +12,48 @@ def get_trade_url_list(html_content):
         temp = etree.HTML(html_content)
         find = temp.xpath("//div[@class='s1ipci53-0 iFQrj']/a[@href]//@href")
         for cur_find in find:
-            btc_trade_list_url.append(config.SITE_MAIN_ADDR["binance"] + cur_find)
+            btc_trade_list_url.append(cur_find)
         return btc_trade_list_url
     except :
         log.error("get btc trade list from binance error")
         return btc_trade_list_url
 
 
-def get_coin_data_from_url(url):
+def get_coin_data_from_tradecoin(tradecoin):
+    tradecoin = tradecoin.split("/")[-1]
+    names = tradecoin.split('_')
+    tradecoin = tradecoin.replace('_', '')
     connPool = urllib3.PoolManager()
-    http = connPool.request(method='get', url=url)
-    html = etree.HTML(http.data)
-    cur_node = html.xpath("//div[@class='s1d711xa-1 jgsWFi s1p4en3j-0 ghSPYW s62mpio-0 ecRduS']/div")[0]
-    cur_node = cur_node.xpath("/span")
-    print(cur_node)
+    deep_url = "https://www.binance.com/api/v1/depth?symbol=" + tradecoin
+    # 1min kline
+    kline_url = "https://www.binance.com/api/v1/klines?symbol="+tradecoin+"&interval=1m"
+    deep_data = eval(connPool.request(method='get', url=deep_url).data)
+    kline_data = eval(connPool.request(method='get', url=kline_url).data)
+    connPool.clear()
+    coin_name = names[0]
+    trade_coin_name = names[1]
+    sell_list = []
+    buy_list = []
+    kline_list = []
+    for cur_sell in deep_data["asks"]:
+        cur_price = cur_sell[0]
+        cur_num = cur_sell[1]
+        cur_deep_unit = common.DeepUnit(cur_price, cur_num)
+        sell_list.append(cur_deep_unit)
+    for cur_buy in deep_data["bids"]:
+        cur_price = cur_buy[0]
+        cur_num = cur_buy[1]
+        cur_deep_unit = common.DeepUnit(cur_price, cur_num)
+        buy_list.append(cur_deep_unit)
+    for cur_kline in kline_data:
+        cur_high = cur_kline[2]
+        cur_low = cur_kline[3]
+        cur_close = cur_kline[4]
+        cur_kline_unit = common.KlineUnit(high=cur_high, low=cur_low, close=cur_close)
+        kline_list.append(cur_kline_unit)
+    cur_price = kline_data[-1][4]
+    cur_data = common.CoinData(coin_name, trade_coin_name, buy_list, sell_list, cur_price, kline_list)
+    return cur_data
 
 
 def get_data():
@@ -35,23 +64,32 @@ def get_data():
     connPool.clear()
 
     # get bnb trade list
-    file = open("d:/coin/coinInfo/DataMinner/binance/binanceBnb.txt", encoding="utf-8")
+    file = open("binanceBnb.txt", encoding="utf-8")
     content = file.read()
     bnb_trade_list = get_trade_url_list(content)
 
     # get eth trade list
-    file = open("d:/coin/coinInfo/DataMinner/binance/binanceEth.txt", encoding="utf-8")
+    file = open("binanceEth.txt", encoding="utf-8")
     content = file.read()
     eth_trade_list = get_trade_url_list(content)
 
     # get usdt trade list
-    file = open("d:/coin/coinInfo/DataMinner/binance/binanceUsdt.txt", encoding="utf-8")
+    file = open("binanceUsdt.txt", encoding="utf-8")
     content = file.read()
     usdt_trade_list = get_trade_url_list(content)
 
-    for cur_btc_coin_url in btc_trade_list:
-        get_coin_data_from_url(cur_btc_coin_url)
-
+    result = []
+    for cur_btc_coin in btc_trade_list + bnb_trade_list + eth_trade_list + usdt_trade_list:
+        try:
+            cur_data = get_coin_data_from_tradecoin(cur_btc_coin)
+        except:
+            log.error("getdata error of:  " + cur_btc_coin)
+        else:
+            log.info("getdata success of:" + cur_btc_coin)
+            print(cur_btc_coin + ":: get data OK")
+            result.append(cur_data)
+    return result
 
 if __name__ == "__main__":
-    get_data()
+    result = get_data()
+    print(len(result))
--- DataMinner/common.py
+++ DataMinner/common.py
@@ -1,13 +1,21 @@
-class deep_unit:
+class DeepUnit:
     def __init__(self, price, num):
         self.price = price
         self.num = num
 
 
+class KlineUnit:
+    def __init__(self, high, low, close):
+        self.high = high
+        self.low = low
+        self.close = close
+
+
 class CoinData:
-    def __int__(self, coin_name, trade_coin_name, buy_list, sell_list, cur_price):
+    def __init__(self, coin_name, trade_coin_name, buy_list, sell_list, cur_price, kline_list):
         self.coin_name = coin_name
         self.trade_coin_nam = trade_coin_name
         self.buy_list = buy_list
         self.sell_list = sell_list
         self.cur_price = cur_price
+        self.kline_list = kline_list
