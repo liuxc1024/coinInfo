54893619ca1b44a6245eccd9ff01e3d9c9a36eec
liuxc2014 <xiangcenliu1992@163.com>
2018-06-17 01:39:30 +0800

    zb is ok

--- DataMinner/main.py
+++ DataMinner/main.py
@@ -1,12 +1,14 @@
 import DataMinner.binance.binance as binance
 import DataMinner.huobi.huobi as huobi
 import DataMinner.gate.gate as gate
+import DataMinner.zb.zb as zb
 
 
 def get_data():
     huobi_result = huobi.get_data()
     gate_result = gate.get_data()
     bin_result = binance.get_data()
+    zb_result = zb.get_data()
 
 if __name__ == "__main__":
     get_data()
---
+++ DataMinner/zb/zb.py
@@ -0,0 +1,66 @@
+from lxml import etree
+import DataMinner.config as config
+import urllib3
+import logging
+import time
+log = logging.getLogger("zbDataGet")
+import DataMinner.common as common
+
+
+def get_data():
+    result = []
+    conn_pool = urllib3.PoolManager()
+    pairs_url = "http://api.zb.com/data/v1/markets"
+    con = conn_pool.request(method='get', url=pairs_url)
+    con = eval(con.data)
+
+    all_price_url = "http://api.zb.com/data/v1/allTicker"
+    all_price_con = eval(conn_pool.request(method='get', url=all_price_url).data)
+    total_num = len(con.keys())
+    cur_num2 = 0
+    for cur_pair in con.keys():
+        cur_num2 = cur_num2 + 1
+        names = cur_pair.split('_')
+        coin_name = names[0]
+        trade_coin_name = names[1]
+        cur_price = all_price_con[coin_name+trade_coin_name]['last']
+        deep_url = "http://api.zb.com/data/v1/depth?market=" + coin_name + "_" + trade_coin_name + "&size=20"
+        deep_http = conn_pool.request(method='get', url=deep_url)
+        deep_con = eval(deep_http.data)
+        sell_list = []
+        for cur_sell in deep_con['asks']:
+            cur_price = cur_sell[0]
+            cur_num = cur_sell[1]
+            cur_sell_unit = common.DeepUnit(price=cur_price, num=cur_num)
+            sell_list.append(cur_sell_unit)
+
+        buy_list = []
+        for cur_buy in deep_con['bids']:
+            cur_price = cur_buy[0]
+            cur_num = cur_buy[1]
+            cur_buy_unit = common.DeepUnit(price=cur_price, num=cur_num)
+            buy_list.append(cur_buy_unit)
+
+        # get kline
+        kline_url = "http://api.zb.com/data/v1/kline?market="+coin_name+"_"+trade_coin_name+"&type=1min"
+        con = eval(conn_pool.request(method='get', url=kline_url).data)
+        kline_list = []
+        time.sleep(1)
+        try:
+            con['data']
+        except:
+            time.sleep(5)
+            con = eval(conn_pool.request(method='get', url=kline_url).data)
+        for cur_k in con['data']:
+            high = cur_k[2]
+            low = cur_k[3]
+            close = cur_k[4]
+            cur_k_unit = common.KlineUnit(high=high, low=low, close=close)
+            kline_list.append(cur_k_unit)
+        cur_data = common.CoinData(coin_name, trade_coin_name, buy_list, sell_list, cur_price, kline_list)
+        log.warning("zb:" + coin_name + ":" + trade_coin_name + ":  " + str(cur_num2) + "/" + str(total_num))
+        result.append(cur_data)
+    return result
+
+if __name__ == "__main__":
+    get_data()
